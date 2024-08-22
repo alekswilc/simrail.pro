5f793aea4bb3a29973ebf57fd7aba6975c09a3bb
alekswilc <aleks@alekswilc.dev>
2024-08-22 19:33:57 +0200

    add migration script, remove possibility to take down website

--- src/http/server.ts
+++ src/http/server.ts
@@ -28,25 +28,6 @@ export class ApiModule {
         app.use('/profiles/', ProfilesRoute.load());
         app.use('/leaderboard/', LeaderboardRoute.load())
 
-
-        app.get('/migrate', async () => {
-            const _ = await MProfile.find({});
-
-            _.forEach(async(x) => {
-                x.trainPoints = Object.values(x.trainStats).length === 1 ?
-                Object.values(x.trainStats)[0].score : 
-                Object.values(x.trainStats).reduce((acc, curr) => acc + curr.score, 0)
-
-                x.trainDistance = Object.values(x.trainStats).length === 1 ?
-                Object.values(x.trainStats)[0].distance : 
-                Object.values(x.trainStats).reduce((acc, curr) => acc + curr.distance, 0)
-
-                await MProfile.updateOne({ id: x.id }, { trainPoints: x.trainPoints, trainDistance: x.trainDistance })
-            })
-
-            
-        })
-
         app.listen(2005);
     }
 }
