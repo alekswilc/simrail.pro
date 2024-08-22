b879999ff05192b8f14acba65ceb1c0e294f4e8b
alekswilc <aleks@alekswilc.dev>
2024-08-22 22:12:30 +0200

    added version & commit to footer

---
+++ src/util/git.ts
@@ -0,0 +1,38 @@
+import { execSync } from 'child_process';
+
+export class GitUtil {
+    private static cache: { lastUpdated: number, version: string | null, commit: string | null } = undefined!;
+
+    public static getLatestVersion() {
+        try {
+            const data = execSync('git describe --tags --exact-match').toString();
+            return data.replace('\n', '');
+        } catch {
+            return null;
+        }
+    }
+
+    public static getLatestCommit() {
+        try {
+            const data = execSync('git rev-parse --short HEAD').toString();
+            return data.replace('\n', '');
+        } catch {
+            return null;
+        }
+    }
+
+
+    public static getData() {
+        if (this.cache && (this.cache.lastUpdated - Date.now()) < 30_000) 
+            return this.cache;
+        
+        const data = {
+            version: this.getLatestVersion(),
+            commit: this.getLatestCommit(),
+            lastUpdated: Date.now()
+        };
+
+        this.cache = data;
+        return data;
+    }
+}
