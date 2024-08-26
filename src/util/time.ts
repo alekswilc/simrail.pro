00d10a1649d301103fd1f25daa779f93d23ca9ae
alekswilc <aleks@alekswilc.dev>
2024-08-26 20:58:11 +0200

    add days to time format

--- src/util/time.ts
+++ src/util/time.ts
@@ -15,9 +15,12 @@ export const msToTime = (duration: number, long = false) => {
 
     if (long) {
         let t = '';
+        if (time.days()) t+= `${time.days()}d `;
         if (time.hours()) t+= `${time.hours()}h `;
         if (time.minutes()) t+= `${time.minutes()}m `;
         if (time.seconds()) t+= `${time.seconds()}s`;
+
+        if (!t) t = '1s';
 
         return `${time.humanize()} (${t})`;
     }
