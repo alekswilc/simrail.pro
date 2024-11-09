df77a343fc26e0a4ad565f6fa71688c16961bea2
alekswilc <aleks@alekswilc.dev>
2024-11-09 23:32:33 +0100

    docs(): license changes

--- packages/frontend/src/components/mini/sidebar/Sidebar.tsx
+++ packages/frontend/src/components/mini/sidebar/Sidebar.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Aleksander <alekswilc> Wilczyński (aleks@alekswilc.dev)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * See LICENSE for more.
+ */
+
 import React, { useEffect, useRef, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import SidebarLinkGroup from "./SidebarLinkGroup.tsx";
--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Aleksander <alekswilc> Wilczyński (aleks@alekswilc.dev)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * See LICENSE for more.
+ */
+
 import React, { useEffect, useState } from "react";
 import { useTranslation, Trans } from "react-i18next";
 import { Link } from "react-router-dom";
@@ -91,7 +107,7 @@ export const Home: React.FC = () =>
                                         } }
                                 /></p>
                                 <p>{ t("home.footer.license") } <Link className="color-orchid"
-                                                                      to={ "https://git.alekswilc.dev/simrail/simrail.alekswilc.dev/src/branch/main/LICENSE" }>GNU
+                                                                      to={ "/LICENSE" }>GNU
                                     AGPL V3</Link></p>
                                 <p>{ t("home.footer.powered") } <Link className="color-orchid"
                                                                       to={ "https://tailadmin.com/" }>TailAdmin</Link>
