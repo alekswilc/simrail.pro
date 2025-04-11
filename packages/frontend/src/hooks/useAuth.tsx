0f8b1a4729e684f4b93ede50baa90cb445bba90d
alekswilc <aleks@alekswilc.dev>
2025-04-11 16:48:47 +0200

    update license year.

--- packages/frontend/src/hooks/useAuth.tsx
+++ packages/frontend/src/hooks/useAuth.tsx
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025 Aleksander <alekswilc> Wilczyński (aleks@alekswilc.dev)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * See LICENSE for more.
+ */
+
+
 import { useContext, createContext, ReactNode } from "react";
 import useSWR from "swr";
 import { get } from "../util/fetcher.ts";
